6e7d1e3208a66f32d2c97444a189af1dce81489c
julio cesar <juliocgcj@hotmail.com>
2022-11-25 12:27:40 -0300

    feat: implements new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
-import { Navbar } from './components/navbar/Navbar';
+import { Navbar } from './components/estaticos/navbar/Navbar';
+import { About } from './paginas/about/About';
+import { Home } from './paginas/home/Home';
 
 
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,8 @@
+import Racte from 'react';
+import './Home.css';
+
+export function Home() {
+    return (
+        <h1>Página Home</h1>
+    )
+}
